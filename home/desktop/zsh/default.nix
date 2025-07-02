7011973bc41c34604d4d9552d34646173cf999f9
Tristan Reynders <tristan.reynders@icloud.com>
2025-07-02 21:29:02 +0200

    Change terminal padding

--- home/desktop/zsh/default.nix
+++ home/desktop/zsh/default.nix
@@ -17,7 +17,7 @@
   programs.starship = {
     enable = true;
     settings = {
-      add_newline = true;
+      add_newline = false;
       character = {
         success_symbol = "[](bold green) ";
         error_symbol = "[✗](bold red) ";
