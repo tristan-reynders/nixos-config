68050f1d6816fc5a7368631da0884819356044ef
Tristan Reynders <tristan.reynders@icloud.com>
2025-07-03 01:05:52 +0200

    Add firefox

--- home/default.nix
+++ home/default.nix
@@ -10,11 +10,11 @@
   home.homeDirectory = "/home/tristan";
   home.stateVersion = "25.05";
   
-  programs.home-manager.enable = true;  
+  programs.home-manager.enable = true; 
 
   home.packages = with pkgs; [
+    adwaita-icon-theme
     greetd.tuigreet
-    firefox
     brightnessctl
     vscode
     nerd-fonts.jetbrains-mono
