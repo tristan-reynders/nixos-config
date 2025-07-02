6cd991acc6b22a53bdecb2be9db4c6b998b74f3a
Tristan Reynders <tristan.reynders@icloud.com>
2025-07-02 13:57:24 +0200

    Update ghostty

--- home/desktop/ghostty/default.nix
+++ home/desktop/ghostty/default.nix
@@ -4,7 +4,6 @@
   programs.ghostty = {
     enable = true;
     settings = {
-      theme = "catppucin-mocha";
       font-family = "JetbrainsMono Nerd Font";
       font-size = 14;
       window-padding-x = 10;
